88e362136e30057fcc150a2e61022c5b3c4d5e87
amrmanoj <110924608+amrmanoj@users.noreply.github.com>
2023-03-16 15:47:43 +0530

    Update Test1.java

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -3,6 +3,7 @@ public class Test1
     public static void main(String[] args)
     {
         System.out.println("Hello Java");
+        System.out.println("Hello Eclipse changed from GitHub");
 
     }
 }
